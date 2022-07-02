a87365abd4638437f2e4f504dd1a31406a82a995
안재영 <88701220+TheLeaderAhn@users.noreply.github.com>
2022-07-02 21:44:43 +0900

    First Xcode source init
    첫 프로젝트 생성

---
+++ 1.grammar/Xcode/MyFirstApp/MyFirstApp/MyFirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppApp.swift
+//  MyFirstApp
+//
+//  Created by 안재영 on 2022/07/02.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
